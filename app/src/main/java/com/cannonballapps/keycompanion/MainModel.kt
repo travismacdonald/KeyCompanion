80b7dfd708b01be9cb5674aaae3a422327c08670
travismacdonald <x2015khn@stfx.ca>
2019-07-09 05:25:57 -0300

    Created MainModel. Added static vars with note names

---
+++ app/src/main/java/com/cannonballapps/keycompanion/MainModel.kt
@@ -0,0 +1,11 @@
+package com.cannonballapps.keycompanion
+
+class MainModel {
+    companion object {
+        val numNotes = 12
+        val notesFlat = arrayOf("C", "D𝄬", "D", "E𝄬", "E", "F", "G𝄬", "G", "A𝄬", "A", "B𝄬", "B")
+        val notesSharp = arrayOf("C", "C𝄰", "D", "D𝄰", "E", "F", "F𝄰", "G", "G𝄰", "A", "A𝄰", "B")
+    }
+
+    
+}
