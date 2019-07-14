7ad016bad50de06226d807ef571d0398b516968a
travismacdonald <x2015khn@stfx.ca>
2019-07-14 19:34:03 -0300

    Added comments in Key class

--- app/src/main/java/com/cannonballapps/keycompanion/Key.kt
+++ app/src/main/java/com/cannonballapps/keycompanion/Key.kt
@@ -1,18 +1,42 @@
 package com.cannonballapps.keycompanion
 
+/**
+ * Class contains two different spellings; flat and sharp.
+ * Some cases it may be the same spelling (flat = C; sharp = C)
+ * Other keys contain two different spellings (flat = C#; Db)
+ */
 class Key(nameFlat: String, nameSharp: String) {
 
+    /**
+     * Array of possible name spellings.
+     */
     private val nameArr = arrayOf(nameFlat, nameSharp)
+
+    /**
+     * Index of current name spelling.
+     * 0 = flat; 1 = sharp.
+     */
     private var nameIx = 0
-    // Set to flat by default.
+
+    /**
+     * Current name spelling.
+     * No setter, can only be changed by functions listed below.
+     */
     var curName = nameArr[nameIx]
         private set
 
+    /**
+     * Toggles name between sharp and flat.
+     * Used when user wants to change current spelling of key.
+     */
     fun toggleName() {
         nameIx = (nameIx + 1) % nameArr.size
         curName = nameArr[nameIx]
     }
 
+    /**
+     * Set name to sharp spelling if flat.
+     */
     fun setNameFlat() {
         if (nameIx != 0) {
             nameIx = 0
@@ -20,6 +44,9 @@ class Key(nameFlat: String, nameSharp: String) {
         }
     }
 
+    /**
+     * Set name to flat spelling if sharp.
+     */
     fun setNameSharp() {
         if (nameIx != 1) {
             nameIx = 1
