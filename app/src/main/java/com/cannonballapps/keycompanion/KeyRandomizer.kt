75e60c6f8e7e9fa9f4fa684a0e6ce3844362d37e
travismacdonald <x2015khn@stfx.ca>
2019-07-14 19:43:04 -0300

    Added Comments in KeyRandomizer

--- app/src/main/java/com/cannonballapps/keycompanion/KeyRandomizer.kt
+++ app/src/main/java/com/cannonballapps/keycompanion/KeyRandomizer.kt
@@ -2,38 +2,70 @@ package com.cannonballapps.keycompanion
 
 class KeyRandomizer {
     companion object {
+        /**
+         * Total number of keys.
+         */
         const val NUM_NOTES = 12
+
+        /**
+         * Flat spelling of key names.
+         */
         val notesFlat = arrayOf("C", "D𝄬", "D", "E𝄬", "E", "F", "G𝄬", "G", "A𝄬", "A", "B𝄬", "B")
+
+        /**
+         * Sharp spelling of key names.
+         */
         val notesSharp = arrayOf("C", "C𝄰", "D", "D𝄰", "E", "F", "F𝄰", "G", "G𝄰", "A", "A𝄰", "B")
     }
 
+    /**
+     * Stores list of key objects (ordered).
+     */
     private var curKeyList = constructKeyList()
 
+    /**
+     * Get key matching index given.
+     */
     fun getKey(ix: Int) : Key {
         return curKeyList[ix]
     }
 
+    /**
+     * Randomizes order of key list.
+     */
     fun shuffleKeyList() {
         curKeyList.shuffle()
     }
 
-    // toggleKey
-    fun toggleKey(toToggle: Key) {
+    /**
+     * Calls keys toggle method.
+     */
+    fun toggleKeySpelling(toToggle: Key) {
         toToggle.toggleName()
     }
 
-    fun setAllFlat() {
+    /**
+     * Sets all keys spelling to flat.
+     */
+    fun setAllSpellingFlat() {
         for (key in curKeyList) {
             key.setNameFlat()
         }
     }
 
-    fun setAllSharp() {
+    /**
+     * Sets all keys spelling to sharp.
+     */
+    fun setAllSpellingSharp() {
         for (key in curKeyList) {
             key.setNameSharp()
         }
     }
 
+    /**
+     * Inflates key list with 12 key objects (ordered).
+     * Default spelling is flat.
+     */
     private fun constructKeyList() : MutableList<Key> {
         val keyList = mutableListOf<Key>()
         for (i in 0 until NUM_NOTES) {
