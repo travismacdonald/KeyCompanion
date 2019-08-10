e0ff8f40b1cc8cd3a213607f906167f8bf6a21b4
travismacdonald <x2015khn@stfx.ca>
2019-08-10 07:29:37 -0300

    Added key grid to fragment

--- app/src/main/java/com/cannonballapps/keycompanion/keys/KeysFragment.kt
+++ app/src/main/java/com/cannonballapps/keycompanion/keys/KeysFragment.kt
@@ -2,16 +2,16 @@ package com.cannonballapps.keycompanion.keys
 
 
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
 import com.cannonballapps.keycompanion.Key
 
-import com.cannonballapps.keycompanion.R
 import java.lang.StringBuilder
+import com.cannonballapps.keycompanion.R
 
 /**
  * A simple [Fragment] subclass.
@@ -19,10 +19,11 @@ import java.lang.StringBuilder
  */
 class KeysFragment : Fragment(), KeysContract.View {
 
+    private lateinit var keysGridContainer: ConstraintLayout
     // Will call presenter methods through this var.
     override lateinit var presenter: KeysContract.Presenter
 
-    private lateinit var keysStr: TextView
+    private lateinit var keysGrid: ConstraintLayout
     private lateinit var randomizeButton: Button
 
     // Todo: probably delete this function
@@ -33,7 +34,6 @@ class KeysFragment : Fragment(), KeysContract.View {
     override fun showAllKeys(keyList: MutableList<Key>) {
         val sb = StringBuilder()
         for (key in keyList) sb.append("${key.curName} ")
-        keysStr.text = sb.toString()
     }
 
     override fun onResume() {
@@ -45,11 +45,15 @@ class KeysFragment : Fragment(), KeysContract.View {
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         val root = inflater.inflate(R.layout.keys_frag, container, false)
+        keysGridContainer = root.findViewById(R.id.keys_grid)
+        keysGrid = inflater.inflate(R.layout.keys_item, root as ViewGroup, false) as ConstraintLayout
+        keysGridContainer.addView(keysGrid)
+
         with (root) {
-            keysStr = findViewById(R.id.keys_text)
             randomizeButton = findViewById(R.id.randomize_button)
             randomizeButton.setOnClickListener { presenter.randomizeKeys() }
         }
+
         return root
     }
 
