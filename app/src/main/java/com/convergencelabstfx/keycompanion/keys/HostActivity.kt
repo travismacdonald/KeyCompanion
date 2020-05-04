dcc051734560ba6f6cb142bea6f145e333f51d0b
travismacdonald <x2015khn@stfx.ca>
2020-05-04 16:34:58 -0300

    Added layout file for Host Activity and imported databinding library

--- app/src/main/java/com/convergencelabstfx/keycompanion/keys/HostActivity.kt
+++ app/src/main/java/com/convergencelabstfx/keycompanion/keys/HostActivity.kt
@@ -1,4 +1,13 @@
 package com.convergencelabstfx.keycompanion.keys
 
-class HostActivity {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.convergencelabstfx.keycompanion.R
+
+class HostActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_host)
+    }
 }
